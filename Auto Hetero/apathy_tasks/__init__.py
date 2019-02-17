ba4353bbe196d7fc60c6f7ac63273678afc82fc0
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2019-02-17 21:06:09 +0100

    Remove unused file

--- Auto Hetero/apathy_tasks/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import counter
-from . import circle
-from .Auto import Auto
-from .Hetero import Hetero
-from .SpatialeFacile import SpatialeFacile
-from .SpatialeDifficile import SpatialeDifficile
